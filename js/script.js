8c6b58b9b6bf61f7548be87bcda8c47032f6ab0f
Aeguy353 <jeffrey.wadkins1@gmail.com>
2025-05-28 16:20:50 -0400

    9

--- js/script.js
+++
@@ -1,8 +0,0 @@
-
-document.getElementById('searchForm').addEventListener('submit', function(e) {
-  e.preventDefault();
-  alert('Live search coming soon!');
-});
-function trustSite() {
-  alert('We will search across all trusted stores for you!');
-}
---
+++ js/scripts.js
@@ -0,0 +1,7 @@
+
+document.getElementById("compareBtn").addEventListener("click", function() {
+  alert("Live search coming soon!");
+});
+document.getElementById("trustBtn").addEventListener("click", function() {
+  alert("We'll choose the best stores for you!");
+});
