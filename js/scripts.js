0f347a56ecb2ecb0ec3ed8ef676a60c5f97d0767
Aeguy353 <jeffrey.wadkins1@gmail.com>
2025-05-28 16:34:09 -0400

    40

--- js/scripts.js
+++
@@ -1,7 +0,0 @@
-
-document.getElementById("compareBtn").addEventListener("click", function() {
-  alert("Live search coming soon!");
-});
-document.getElementById("trustBtn").addEventListener("click", function() {
-  alert("We'll choose the best stores for you!");
-});
---
+++ script/main.js
@@ -0,0 +1,7 @@
+
+function runLiveSearch() {
+    alert("Live CJ API product search initiated... [real search logic goes here]");
+}
+function trustUsSearch() {
+    alert("Trusting GetComparley to auto-select the best stores...");
+}
