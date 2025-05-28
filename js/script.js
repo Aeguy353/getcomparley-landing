8150482a76cff4c48202a39a8db5b94dabcf5268
Aeguy353 <jeffrey.wadkins1@gmail.com>
2025-05-28 15:06:40 -0400

    28

--- js/script.js
+++ js/script.js
@@ -1,4 +1,8 @@
 
-function searchProducts() {
-    alert('Live product comparison will be available soon.');
+document.getElementById('searchForm').addEventListener('submit', function(e) {
+  e.preventDefault();
+  alert('Live search coming soon!');
+});
+function trustSite() {
+  alert('We will search across all trusted stores for you!');
 }
